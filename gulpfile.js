c67a63c59445f7f2f0dec8d2908137538afad19c
heense <hrmadsen89@gmail.com>
2017-06-18 21:30:44 +0200

    Adding less version

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var sass        = require('gulp-less');
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src(['app/scss/*.scss'])
+gulp.task('less', function() {
+    return gulp.src(['app/less/*.less'])
         .pipe(sass())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['less'], function() {
 
     browserSync.init({
         server: "./app"  
     });
 
-    gulp.watch(['app/scss/*.scss'], ['sass']);
+    gulp.watch(['app/less/*.less'], ['less']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
